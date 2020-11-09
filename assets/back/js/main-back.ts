8906b892c5dd3dbffd4aa7f972ffb1f11a8779bd
n3wborn <n3wborn@protonmail.com>
2020-11-09 16:24:27 +0100

    [Feature] Add button to handle new book checkbox

--- assets/back/js/main-back.ts
+++ assets/back/js/main-back.ts
@@ -1,7 +1,39 @@
 require('./adminLogin');
 
+// add_book checkbox / button
+const chkBox:HTMLElement = document.getElementById('book_new');
+const newBtn:HTMLElement = document.getElementById('book_newbtn');
+
+
 // Date actuelle en français
 var options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+
+
+// Function to toggle checked state of a checkbox
+function toggleCheck(el) {
+    if (el.checked === false) {
+        el.checked = true;
+        el.value = 1;
+        return innerChange(newBtn, 'Livre inédit !');
+    } else {
+        el.checked = false;
+        el.value = 0;
+        return innerChange(newBtn, 'Livre déjà édité');
+    }
+}
+
+// Function to replace el element innnerHTML value with string value
+function innerChange(el, string) {
+    el.innerHTML = '';
+    return el.innerHTML = string;
+}
+
+// flip from true/false 1/0
+newBtn.addEventListener('click', function () {
+    toggleCheck(chkBox);
+});
+
+
 document.querySelector('.dateActuelle').textContent = `${new Date(Date.now()).toLocaleDateString('fr-FR', options)}`;
 
 let burger = document.querySelector(".hamburger");
