49ab6293cbe9d70f00e326e499457f20a4b76f1b
n3wborn <n3wborn@protonmail.com>
2020-11-27 10:53:52 +0100

    [Refactor] Add formtype GenreType

---
+++ src/Form/GenreType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Genre;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GenreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Genre',
+                'label_attr' => [
+                    'class' => 'font-weight-bold'
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Genre::class,
+        ]);
+    }
+}
