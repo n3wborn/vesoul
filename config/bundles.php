18ea8a3154f62236eefea215feb14271ceb0b37e
n3wborn <n3wborn@protonmail.com>
2020-11-27 18:34:43 +0100

    [Build] Remove useless web-server-bundle (because Symfo v4.4)

--- config/bundles.php
+++ config/bundles.php
@@ -12,5 +12,4 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
