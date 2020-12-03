79fc70f4f26f05c7685ffe5e7de2c363fd6282a8
n3wborn <n3wborn@protonmail.com>
2020-12-03 16:13:44 +0100

    [Refactor] Add ChangePasswordType.php form type

---
+++ src/Form/ChangePasswordType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ChangePasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('currentPassword', PasswordType::class, [
+                'constraints' => [
+                    new UserPassword(),
+                ],
+                'label' => false,
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'placeholder' => "Current password"
+                ],
+            ])
+            ->add('newPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'constraints' => [
+                    new NotBlank(),
+                    new Length([
+                        'min' => 5,
+                        'max' => 128,
+                    ]),
+                ],
+                'first_options' => [
+                    'label' => false,
+                    'attr' => [
+                        'placeholder' => 'New Password'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => false,
+                    'attr' => [
+                        'placeholder' => 'New Password'
+                    ]
+                ],
+            ])
+        ;
+    }
+}
