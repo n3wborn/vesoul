ee7794667130802f569acc98fd847ec0d22cc593
n3wborn <n3wborn@protonmail.com>
2020-12-03 16:53:47 +0100

    [Refactor] Add CommandType form

---
+++ src/Form/CommandType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Address;
+use App\Entity\Command;
+use App\Repository\AddressRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Security;
+
+class CommandType extends AbstractType
+{
+    private Security $security;
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(Security $security, EntityManagerInterface $entityManager)
+    {
+        $this->security = $security;
+        $this->entityManager = $entityManager;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $user = $this->security->getUser();
+
+
+
+        $builder
+            ->add('livraison', EntityType::class, [
+                'class' => Command::class,
+                'choice_label' => function (Address $address){
+                    dd($address->getTitle());
+                    return $address->getTitle();
+                },
+                'query_builder' => function (EntityRepository $repository) use ($user) {
+                    return $repository->createQueryBuilder('a')
+                        ->where('a.user = :user')
+                        ->setParameter('user', $user)
+                    ;
+                }
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Command::class,
+        ]);
+    }
+}
