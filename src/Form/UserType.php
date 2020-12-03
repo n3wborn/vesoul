056d451acc57d708af570c369454829fb71b8fc3
n3wborn <n3wborn@protonmail.com>
2020-12-03 16:48:27 +0100

    [Refactor] Add UserType class

---
+++ src/Form/UserType.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'label' => 'Email',
+                'label_attr' => [
+                    'class' => 'font-weight-bold'
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
+            ->add('gender', ChoiceType::class, [
+                'data' => 0,
+                'choices' => [ 'Homme' => 0, 'Femme' => 1],
+                'label' => 'Genre',
+                'label_attr' => [
+                    'class' => 'font-weight-bold'
+                ],
+                'attr' => [
+                    'class' => 'form-control custom-select',
+                ]
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'label_attr' => [
+                    'class' => 'font-weight-bold'
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'label_attr' => [
+                    'class' => 'font-weight-bold'
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
+            ->add('tel', TextType::class, [
+                'label' => 'Téléphone',
+                'label_attr' => [
+                    'class' => 'font-weight-bold'
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
