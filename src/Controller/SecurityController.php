b124653cfc4bb7584be92c996a1d979481ffb466
n3wborn <n3wborn@protonmail.com>
2020-12-07 17:06:26 +0100

    [Feature] Update user login / Add user authentification

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,24 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Swift_Mailer;
+use Swift_Message;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
 
     private AuthenticationUtils $authenticationUtils;
+    private UserPasswordEncoderInterface $encoder;
+    private Swift_Mailer $mailer;
+    private EntityManagerInterface $manager;
 
-
-    public function __construct(AuthenticationUtils $authenticationUtils)
+    public function __construct(
+        AuthenticationUtils $authenticationUtils,
+        UserPasswordEncoderInterface $encoder,
+        Swift_Mailer $mailer,
+        EntityManagerInterface $manager
+    )
     {
         $this->authenticationUtils = $authenticationUtils;
+        $this->encoder = $encoder;
+        $this->mailer = $mailer;
+        $this->manager = $manager;
     }
 
     /**
-     * @Route("/login", name="login")
+     * @Route("/connexion", name="login")
      */
     public function login(): Response
     {
@@ -39,9 +56,58 @@ class SecurityController extends AbstractController
 
 
     /**
-     * @Route("/logout", name="logout")
+     * @Route("/deconnexion", name="logout")
      */
     public function logout()
     {
+    }
+
+
+    /**
+     * @Route("/inscription", name="registration")
+     */
+    public function registration(Request $request)
+    {
+
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user)
+            ->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $contact = $form->getData();
+
+            // if form is ok : hash user's password and save infos
+            $hash = $this->encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hash)->setRoles(['ROLE_USER']);
+
+            $this->manager->persist($user);
+            $this->manager->flush();
+
+            // Confirm subscription
+            $this->addFlash('success', "Félicitation ! Un email de confirmation vous a été envoyé");
+
+            // and confirm va email
+            $mail = (new Swift_Message("Bienvenue sur Vesoul Edition !"))
+                ->setFrom($user->getUsername())
+                // TODO: Replace by site email
+                ->setBody(
+                    $this->renderView(
+                        'email/confirm.html.twig',
+                        [
+                            'email' => $user->getUsername(),
+                            'contact' => $contact
+                        ]
+                    ),'text/html'
+                );
+
+            // send email, then redirect
+            $this->mailer->send($mail);
+            return $this->redirectToRoute('security_user_login');
+        }
+
+        return $this->render('dashboard-user/inscription.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
