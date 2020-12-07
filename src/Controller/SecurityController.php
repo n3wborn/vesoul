d0a8fea4832088f13f9fb31d47eb294d8ce494d0
n3wborn <n3wborn@protonmail.com>
2020-12-07 16:11:36 +0100

    [Feature WIP] Add user authentification poc

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+
+    private AuthenticationUtils $authenticationUtils;
+
+
+    public function __construct(AuthenticationUtils $authenticationUtils)
+    {
+        $this->authenticationUtils = $authenticationUtils;
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(): Response
+    {
+
+         if ($this->getUser()) {
+             return $this->redirectToRoute('dashboard_user_home');
+         }
+
+        $error = $this->authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $this->authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout()
+    {
+    }
+}
